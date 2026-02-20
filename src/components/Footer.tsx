114a90664245cca19d64fb2df626749969a3c814
Diego Lopes <lopesdiego12@gmail.com>
2026-02-20 16:34:52 +0100

    Remove location details from Footer component
    Removed location information from the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -88,11 +88,6 @@ const Footer = () => {
               that drive business growth.
             </p>
 
-            <div className="space-y-3">
-              <div className="flex items-center space-x-3">
-                <MapPin className="w-5 h-5 text-primary-600" />
-                <span className="text-gray-300">Den Haag, Netherlands</span>
-              </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-primary-600" />
                 <a
